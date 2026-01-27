c044e06517fa481762946bca087c8f4347253021
Sai Chaitanya Gaddam <saichithanya1@gmail.com>
2026-01-28 10:13:13 +1100

    feat: add ClerkProvider for authentication context

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,10 +1,17 @@
+import { AppHeader } from "@/components/app/layout/AppHeader";
+import { OnboardingGuard } from "@/components/app/onboarding/OnboardingGuard";
+
 import { SanityLive } from "@/sanity/lib/live";
 import { ClerkProvider } from '@clerk/nextjs'
 
 function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider>
+      <OnboardingGuard>
+        <AppHeader />
       {children}
+      </OnboardingGuard>
+
       
       <SanityLive />  
     </ClerkProvider>
