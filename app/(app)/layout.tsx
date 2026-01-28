e3aa2b20be7f8e60dab1ca19f089a00fdb187fa2
Sai Chaitanya Gaddam <saichithanya1@gmail.com>
2026-01-28 23:33:00 +1100

    Introduce ChatStoreProvider for global chat state management

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,5 +1,6 @@
 import { AppHeader } from "@/components/app/layout/AppHeader";
 import { OnboardingGuard } from "@/components/app/onboarding/OnboardingGuard";
+import { ChatStoreProvider } from "@/lib/store/chat-store-provider";
 
 import { SanityLive } from "@/sanity/lib/live";
 import { ClerkProvider } from '@clerk/nextjs'
@@ -7,13 +8,13 @@ import { ClerkProvider } from '@clerk/nextjs'
 function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider>
+      <ChatStoreProvider>
       <OnboardingGuard>
         <AppHeader />
       {children}
       </OnboardingGuard>
-
-      
-      <SanityLive />  
+      <SanityLive /> 
+      </ChatStoreProvider> 
     </ClerkProvider>
   )
 }
